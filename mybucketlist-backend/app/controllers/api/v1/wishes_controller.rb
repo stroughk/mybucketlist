f101718d764c6d5a5ba8d58f1818827c27c93a16
stroughk <katerynastrough@hotmail.com>
2020-03-14 20:29:59 -0500

    create actions in controllers and refactor routes

--- mybucketlist-backend/app/controllers/api/v1/wishes_controller.rb
+++ mybucketlist-backend/app/controllers/api/v1/wishes_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::WishesController < ApplicationController
+
+    before_action :set_account
+    
+    def index
+        @wishes = @category.wishes 
+        render json: @wishes
+    end
+
+    def create
+        @wish = @category.wishes.new(wish_params)
+        if @wish.save
+            render json: @wish
+        else
+            render json: {error: 'Sorry, please try again'}
+        end
+    end
+
+    def show
+      @wish = Wish.find(params[:id])
+      render json: @wish 
+    end
+
+    def destroy
+        @wish = Wish.find(params[:id])
+        @wish.destroy
+    end
+
+    private
+
+    def set_account
+        @category = Category.find(params[:category]) #any wish we have it's associated with a category, and we set it based on the category id
+    end
+
+    def wish_params
+        params.require(:wish).permit(:category_id, :content, :fulfilled)
+    end
 end
--- mybucketlist-backend/config/routes.rb
+++ mybucketlist-backend/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :categories do
       resources :wishes
-      resources :categories
+      end
     end
   end
 end
